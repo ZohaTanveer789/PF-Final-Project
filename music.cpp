ad36e2b3e59e9074c12bcc1515b10f9a7ae25880
Zoha Tanveer <zohatanveer2006@gmail.com>
2026-05-23 17:57:44 +0500

    Add music recommender menu in music.cpp
    Implement a basic music recommender menu with options.

--- music.cpp
+++ music.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+int main(){
+    int choice=0;
+    do
+    {
+       cout<<"------------------------------"<<endl;
+       cout<<"Music Recommender"<<endl;
+       cout<<"1- Enter Mood"<<endl;
+       cout<<"2- View Mood History"<<endl;
+       cout<<"3- View Playlist History"<<endl;
+       cout<<"4-Exit"<<endl;
+       cout<<"Enter your choice: ";
+       cin>>choice; 
+       cout<<"------------------------------"<<endl;
+ 
+    } while (choice > 1 && choice < 4); 
+    
+}
+
 
